3077221704f77ab8282b1bd56e3b395af7e90ef7
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2015-01-10 19:36:24 -0300

    Se mejoran comentarios en el código y se emprolija

--- KeyReloaded/Sources/main.asm
+++ KeyReloaded/Sources/main.asm
@@ -65,26 +65,23 @@ KEY2       RMB   3
 ;
 ; Entry point cargado en el vector de interrupciones
 ; (ver MCUinit.inc)
-
 _Startup:
             LDHX   #RAMEnd+1        ; initialize the stack pointer
             TXS
             ; Se invoca MCU_init, definido en MCUinit.inc, generado
             ; por Device Initialization
             JSR    MCU_init
-;
+
 ; Codigo main
-;
 MAIN        BSR   INISYS
 
 ; Bucle infinito
-;
 LOOP:       
             LDA   KEY1+STATE
             AND   #%00000001
             CMP   #1
             BNE   KEY1OFF
-            BSR   LEDON                   ; tecla 1 presionada
+            BSR   LEDON                   ; Tecla 1 presionada
             BRA   NEXT
 KEY1OFF     BSR   LEDOFF
 
@@ -92,7 +89,7 @@ NEXT        LDA   KEY2+STATE
             AND   #%00000001
             CMP   #1
             BNE   KEY2OFF
-            BSR   BUZON                   ; tecla 2 presionada
+            BSR   BUZON                   ; Tecla 2 presionada
             BRA   NEXT1
 KEY2OFF     BSR   BUZOFF
             
@@ -124,21 +121,17 @@ BUZOFF      LDA   #BUZ_MASK
 ;** Se invoca cada 1 ms
 ;** ===================================================================
 TICK_1MS
-            LDHX  #KEY1        ; pongo el parámetro en el stack
+            LDHX  #KEY1       ; Pongo el parámetro (KEY1) en el stack
             PSHX
             PSHH
+            
+            BSR   SMKBD       ; Invoco función de debounce para KEY1
+            AIS   #2          ; Reseteo el stack para desechar parámetro
 
-            LDHX  #$FF
-            BSR   SMKBD
-            AIS   #2          ; reseteo el stack para desechar parámetro
-
-            LDHX  #KEY2        ; pongo el parámetro en el stack
+            LDHX  #KEY2       ; Pongo el parámetro (KEY2) en el stack
             PSHX
             PSHH
-
-            LDHX   #$11            
-            BSR   SMKBD
-
+            BSR   SMKBD       ; Invoco función de debounce para KEY2
             AIS   #2          ; reseteo el stack para desechar parámetro
 
 END_TICK    RTS
@@ -160,20 +153,20 @@ INISYS      CLI             ; habilito interrupciones
             
             RTS
 
-;** ===================================================================
-;** SMKBD         :     Máquina de estados que maneja un pulsador.  Recibe en el 
-;**                     stack un puntero a una variable de 3 bytes que contiene
-;**                     el puerto del pulsador, la máscara en ese puerto, y actualiza
-;**                     el estad que tiene el pulstador.
-;**
-;** PORT          :     Puerto del pulsador.
-;** PIN_MASK      :     Máscara del pin del pulsador.
-;** STATE[0]      :     Bit que indica si el pulsador está presionado o liberado.
-;** STATE[1:2]      :     Estado de la maquina de estados, de uso interno de la rutina.
-;** STATE[3:7]    :     Esta parte del byte se usa para el contador del debounce_delay
-;                       se puede contar hasta 2^5 = 32   
-;** ===================================================================
-SMKBD       PSHX        ; resguardo registros
+;** ================================================================================== **
+;** SMKBD         :     Máquina de estados que maneja un pulsador.  Recibe en el       **
+;**                     stack un puntero a una variable de 3 bytes que contiene        **
+;**                     el puerto del pulsador, la máscara en ese puerto, y actualiza  **
+;**                     el estad que tiene el pulstador.                               **
+;**                                                                                    **
+;** PORT          :     Puerto del pulsador.                                           **
+;** PIN_MASK      :     Máscara del pin del pulsador.                                  **
+;** STATE[0]      :     Bit que indica si el pulsador está presionado o liberado.      **
+;** STATE[1:2]    :     Estado de la maquina de estados, de uso interno de la rutina.  **
+;** STATE[3:7]    :     Esta parte del byte se usa para el contador del debounce_delay **
+;**                     se puede contar hasta 2^5 = 32.  De uso interno de la rutina.  **
+;** ================================================================================== **
+SMKBD       PSHX                    ; resguardo registros
             PSHA
             PSHH
 
@@ -186,70 +179,87 @@ KEY         EQU   (2+3+1)           ; offset del parámetro:
             LDHX  KEY,SP            ; Pongo en H:X la dirección del parámetro
             LDA   STATE,X           ; desreferencio el estado y lo guardo en A
             AND   #%00000110        ; elimino todos los bits menos los del estado
-            CMP   #(WAITING<<1)
+            
+            LSRA                    ; Corro hacia la derecha una posición ya que el
+                                    ; estado está en los bits 1 y 2 del byte STATE
+            
+            CMP   #WAITING          ; Si estado == WAITING
             BEQ   S_WAITING
-            CMP   #(BOUNCE1<<1)
+            
+            CMP   #BOUNCE1          ; Si estado == BOUNCE1
             BEQ   S_BOUNCE1
-            CMP   #(PUSHED<<1)
+            
+            CMP   #PUSHED           ; Si estado == PUSHED
             BEQ   S_PUSHED
-            CMP   #(BOUNCE2<<1)
+            
+            CMP   #BOUNCE2          ; Si estado == BOUNCE2
             BEQ   S_BOUNCE2
 
-            BRA   END_TMP
+            BRA   END_TMP           ; default: estado inválido.  No debería pasar.
+
+; Estado Waiting
 S_WAITING
             LDHX  KEY,SP            ; Pongo en H:X la dirección del parámetro
-            LDX   PORT,X            ; desreferencio el puerto y lo guardo en A
+            LDX   PORT,X            ; Desreferencio el puerto y lo guardo en A
             LDA   ,X
             
             LDHX  KEY,SP            ; Vuelvo a poner en H:X el parámetro
-            AND   MASK,X            ; desreferencio la máscara y hago AND
+            AND   MASK,X            ; Leo el valor de la máscara y hago AND
             CMP  #0                 ; Si el AND da cero, está presionada la key
-            BNE   END_TMP
+            BNE   END_TMP           ; Si no está presionada la key, termino
             
-            LDHX  KEY,SP            ; paso el estado a BOUNCE1
+            LDHX  KEY,SP            ; Paso el estado a BOUNCE1
             LDA   #(BOUNCE1<<1)
             ORA   #(DELAY<<3)       ; pongo el contador en #DELAY
             STA   STATE,X            
             
-END_TMP     BRA   END_TMP2
+END_TMP     BRA   END_TMP2          ; Salto intermedio para llegar a final, por dirección > 127
+
+; Estado Bounce1: sospecha de que se presionó la key
 S_BOUNCE1
-            LDHX  KEY,SP            ; Leo el estado y dejo solo el contador
+            LDHX  KEY,SP            ; Leo el estado.
             LDA   STATE,X
-            AND   #%11111000        ; Dejo solo los bits del contador (7:3)
-            LSRA                    ; corro a la derecha para dejar solo los 5 bits del contador
+            AND   #%11111000        ; Dejo solo los bits del contador (7:3) con máscara AND
+            LSRA                    ; Shift right 3 veces para dejar solo los 5 bits del contador
             LSRA
             LSRA
-            DBNZA END_BNC1          ; Si es cero termino el tiempo de espera, confirmo pulsacion,
+            DBNZA END_BNC1          ; Si es cero termino el tiempo de espera, confirmo si hay pulsacion,
                                     ; pero si aún no es cero tengo que guardar el contador y salir
 
             LDX   PORT,X            ; desreferencio el puerto y lo guardo en A
             LDA   ,X
             
             LDHX  KEY,SP            ; Vuelvo a poner en H:X el parámetro
-            AND   MASK,X            ; desreferencio la máscara y hago AND
+            AND   MASK,X            ; Leo la máscara y hago AND
             CMP  #0                 ; Si el AND da cero, está presionada la key
             BEQ   TO_PUSHED
             
-            LDA   #(WAITING<<1)     ; Si no está presionada, falsa alarma
-            STA   STATE,X
-END_TMP2    BRA   END_SMKBD
+            LDA   #WAITING          ; Si no está presionada, falsa alarma, se vuelve a waiting
+            LSLA                    ; Shift left una posición pues el estado esta en STATE[1:2]
+            STA   STATE,X           ; Guardo estado en memoria
             
-TO_PUSHED   LDA   #(PUSHED<<1)      ; Paso el estado a PUSHED
-            ORA   #1                ; Pongo en 1 el flag de presionado, bit 0
-            STA   STATE,X           ; Guardo estado en memoria.
+END_TMP2    BRA   END_SMKBD         ; Fin de esta parte del switch
+            
+TO_PUSHED   LDA   #PUSHED           ; Paso el estado a PUSHED
+            LSLA                    ; Shift left una posición pues el estado esta en STATE[1:2]
+            
+            ORA   #1                ; Pongo en 1 el flag de presionado, STATE[0]
+            STA   STATE,X           ; Guardo estado en memoria
             BRA   END_SMKBD         ; Finalizo esta parte del switch
 
-END_BNC1    
+END_BNC1                            ; Guardo el contador a memoria para la próxima llamada
             LSLA                    ; Vuelvo a ubicar los bits del contador en posicion 7:3 en STATE
             LSLA
             LSLA
             PSHA                    ; Backup del contador en SP
-            LDA   STATE,X           ; Traigo de memoria todo el state
-            AND   #%00000111        ; Reseteo el contador
-            ORA   1,SP              ; Incorporo el contador backupeado en SP            
-            STA   STATE,X
-            PULA
+            LDA   STATE,X           ; Traigo de memoria todo el STATE y
+            AND   #%00000111        ; pongo en cero el contador para pisar el valor con el decrementado
+            ORA   1,SP              ; Incorporo el contador backupeado en SP con máscara OR
+            STA   STATE,X           ; Almaceno el estado en memoria.
+            AIS   #1                ; Saco del stack el contador almacenado.
             BRA   END_SMKBD
+
+; Estado pushed
 S_PUSHED
             LDHX  KEY,SP            ; Pongo en H:X la dirección del parámetro
             LDX   PORT,X            ; desreferencio el puerto y lo guardo en A
@@ -308,7 +318,7 @@ END_BNC2
 
 END_SMKBD            
             
-            PULH        ; dejo los registros como estaban
+            PULH                    ; Dejo los registros como estaban
             PULA
             PULX
             
